c709a1c5718db684bd9ebf2f7b62d3ddf37ed2cb
Sujan10joshi <47076786+Sujan10joshi@users.noreply.github.com>
2020-06-24 18:41:36 +0545

    Add files via upload

---
+++ lamdas.py
@@ -0,0 +1,23 @@
+# def add(a,b):
+#     return a+b
+# print(add(2,5))
+
+# add1 = lambda a,b : a+b
+# print(add1(4,5))
+
+# multi = lambda x,y: x*y
+# print(multi(3,4))
+
+# is_even = lambda a : a%2 == 0
+# print(is_even(7))
+
+# last_string = lambda s : s[-1]
+# print(last_string('sujan'))
+
+def len_string(l):
+    return len(l) > 5
+
+print(len_string('sujana'))
+
+len_str = lambda l : len(l) > 5
+print(len_string('szn'))
